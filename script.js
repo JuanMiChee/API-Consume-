b4d9af51eaba128dc5047fe7fee9be6549d8e94a
Juan Harrington <harrigtonjuan@outlook.com>
2021-11-01 22:18:44 -0400

    APi consume #1

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+
+async function traerPais(name) {
+    let url = 'https://api.nationalize.io?name=' + name;
+    console.log(url);
+    let response = await fetch(url);
+    return response.json();
+}
+
+
+function mostrarNombre(json) {
+
+    
+    
+
+    if (json.country.legnth == 0){
+
+        let errorMessage ="tu nombre parece no pertenecer a ingun lado";
+        let errorTag = document.getElementById("error");
+        errorTag.innerHTML = errorMessage;        
+        
+    }else{
+
+        let allCountries = json.country.map(function(element) { 
+            return element.country_id 
+        });
+
+    allCountries[0];  
+    let show = document.getElementById("show");
+    show.innerHTML = allCountries[0];
+    }
+}
+function calcular(){
+let nombre = document.getElementById("input").value;
+traerPais(nombre).then(mostrarNombre);
+}
+
+
+
+
+
+
+
